b70611ef4abd71f11675953c9c431ae271798edb
Dharma Putra <dharma66putra@gmail.com>
2024-01-26 06:44:07 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_rust_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_rust_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_rust_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
